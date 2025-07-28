1d738bb7f76f33c9a7eb487e9c25d3ce7ec8cd9f
Dmytrii Datskevych <datskevychdv@gmail.com>
2025-07-28 15:20:57 +0200

    Fix problem after code review

--- src/js/helpers.js
+++ src/js/helpers.js
@@ -1,32 +1,3 @@
-import iziToast from "izitoast";
-import "izitoast/dist/css/iziToast.min.css";
-
-import caution from '../img/icons/caution.svg';
-import error from '../img/icons/error.svg';
-
-export function errorMessage(message) {
-    iziToast.error({
-        messageColor: '#fff',
-        iconColor: '#fff',
-        maxWidth: '350px',
-        iconUrl: error,
-        position: 'topRight',
-        color: '#ef4040',
-        message
-    });
-}
-export function warningMessage(message) {
-    iziToast.warning({
-        messageColor: '#fff',
-        iconUrl: caution,
-        iconColor: '#ffffffff',
-        maxWidth: '350px',
-        position: 'topRight',
-        color: '#ffa000',
-        message
-    });
-}
-
 export function smoothScrollLoadMore() {
     const galleryCard = document.querySelector('.gallery-item');
 
--- src/main.js
+++ src/main.js
@@ -1,77 +1,135 @@
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
 import { getImagesByQuery, params } from "./js/pixabay-api";
 import refs from "./js/refs";
-import { createGallery, showLoader, hideLoader, clearGallery, showLoadMoreButton, hideLoadMoreButton } from './js/render-functions';
-import { smoothScrollLoadMore, errorMessage, warningMessage } from "./js/helpers";
+import {
+    createGallery,
+    showLoader,
+    hideLoader,
+    clearGallery,
+    showLoadMoreButton,
+    hideLoadMoreButton,
+} from "./js/render-functions";
+import { smoothScrollLoadMore } from "./js/helpers";
+import caution from './img/icons/caution.svg';
+import error from './img/icons/error.svg';
 
-let input = null;
+let query = '';
 let page = 1;
 let totalHits = 0;
 
-
-refs.form.addEventListener('submit', handleSubmit);
-refs.loadMoreBtn.addEventListener('click', onLoadMore);
+refs.form.addEventListener("submit", handleSubmit);
+refs.loadMoreBtn.addEventListener("click", handleLoadMore);
 
 async function handleSubmit(event) {
     event.preventDefault();
-    const form = event.target;
-    input = form.elements.search_text.value.trim();
+
+    const searchValue = event.target.elements.search_text.value.trim();
+
+    if (!searchValue) {
+        iziToast.warning({
+            messageColor: '#fff',
+            iconUrl: caution,
+            iconColor: '#ffffffff',
+            maxWidth: '350px',
+            position: 'topRight',
+            color: '#ffa000',
+            message: "You forgot to type your request",
+        });
+        return;
+    }
+
+    query = searchValue;
+    page = 32;
+    totalHits = 0;
+
     clearGallery();
     hideLoadMoreButton();
-    hideLoader();
-
-    if (input) {
-        showLoader();
-        try {
-            const data = await getImagesByQuery(input, page = 1)
-            if (data.hits.length) {
-                createGallery(data.hits);
-                if (data.hits.length < data.totalHits) {
-                    showLoadMoreButton();
-                }
-                totalHits = data.totalHits
-
-            } else {
-                errorMessage('Sorry, there are no images matching your search query. Please try again!');
-            }
-        }
-        catch (err) {
-            console.log(err.message);
-        }
-        finally {
-            hideLoader();
-        };
-    } else {
-        warningMessage('You forgot type your request');
-    }
-    form.reset();
-}
-
-async function onLoadMore() {
     showLoader();
-    hideLoadMoreButton();
-    refs.loadMoreBtn.disabled = true;
 
     try {
-        page++;
-        const data = await getImagesByQuery(input, page)
-        const totalPage = totalHits / params.per_page;
+        const data = await getImagesByQuery(query, page);
 
+        if (!data.hits.length) {
+            iziToast.error({
+                messageColor: '#fff',
+                iconColor: '#fff',
+                maxWidth: '350px',
+                iconUrl: error,
+                position: 'topRight',
+                color: '#ef4040',
+                message: "Sorry, there are no images matching your search query. Please try again!"
+            });
+            return;
+        }
+
+        createGallery(data.hits);
+        totalHits = data.totalHits;
+
+        if (totalHits > params.per_page) {
+            showLoadMoreButton();
+        }
+
+    } catch (error) {
+        iziToast.error({
+            messageColor: '#fff',
+            iconColor: '#fff',
+            maxWidth: '350px',
+            iconUrl: error,
+            position: 'topRight',
+            color: '#ef4040',
+            message: "An error occurred while fetching images"
+        });
+        console.error(error.message);
+    } finally {
+        hideLoader();
+        event.target.reset();
+    }
+}
+
+
+async function handleLoadMore() {
+    page++;
+    refs.loadMoreBtn.disabled = true;
+    hideLoadMoreButton();
+    showLoader();
+
+    try {
+        console.log(page);
+
+        const data = await getImagesByQuery(query, page);
         createGallery(data.hits);
         smoothScrollLoadMore();
 
-        if (page >= totalPage) {
-            hideLoadMoreButton();
-            warningMessage("We're sorry, but you've reached the end of search results.");
+        const totalPages = Math.ceil(totalHits / params.per_page);
+        if (page < totalPages) {
+            showLoadMoreButton();
+        } else {
+            iziToast.warning({
+                messageColor: '#fff',
+                iconUrl: caution,
+                iconColor: '#ffffffff',
+                maxWidth: '350px',
+                position: 'topRight',
+                color: '#ffa000',
+                message: "We're sorry, but you've reached the end of search results."
+            });
         }
-        else {
-            showLoadMoreButton()
-        }
-    }
-    catch (err) {
-        console.log(err.message);
-    }
-    finally {
+
+    } catch (error) {
+        iziToast.error({
+            messageColor: '#fff',
+            iconColor: '#fff',
+            maxWidth: '350px',
+            iconUrl: error,
+            position: 'topRight',
+            color: '#ef4040',
+            message: "Failed to load more images."
+        });
+        console.error(error.message);
+    } finally {
         hideLoader();
         refs.loadMoreBtn.disabled = false;
-    };
+    }
 }
